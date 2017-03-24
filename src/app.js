df274f288c579aef62e20a85a37c502e17c7d2d3
Aaron Strouse <aaronstrouse@gmail.com>
2017-03-24 10:26:01 -0400

    added callback function to app.listen to indicate frontend server is running

--- src/app.js
+++ src/app.js
@@ -6,9 +6,11 @@ var app = express();
 
 // 1st Param: location param for route created for root, no folder or file specified
 // 2nd Param: anonymous callback, and response objects send method sends message to client.
-app.get('/', function(request, response) { 
-    response.send("Aaron rocks");
+app.get('/', function(req, res) { // Most docs show shortened versions
+    res.send("<h1>Aaron rocks</h1>");
 });
-app.listen(3500); // app can be started at this point using node src/app
+app.listen(3500, function(){ //added callback function as 2nd parameter
+    console.log("The frontend server is running port 3500.")
+}); // app can be started at this point using node src/app
 
 // Error of Cannot GET / means no routes have been set up
