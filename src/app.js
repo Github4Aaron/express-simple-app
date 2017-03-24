210ccf1c77dd5f90a3be6ee30db7c62cbd7bd05d
Aaron Strouse <aaronstrouse@gmail.com>
2017-03-24 10:18:05 -0400

    added app.get & app.listen

--- src/app.js
+++ src/app.js
@@ -1,3 +1,14 @@
 'use strict';
 
 var express = require('express');
+
+var app = express(); 
+
+// 1st Param: location param for route created for root, no folder or file specified
+// 2nd Param: anonymous callback, and response objects send method sends message to client.
+app.get('/', function(request, response) { 
+    response.send("Aaron rocks");
+});
+app.listen(3500); // app can be started at this point using node src/app
+
+// Error of Cannot GET / means no routes have been set up
