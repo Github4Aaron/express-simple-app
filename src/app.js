f3975409fb661c1125f647986526a9b4406b107c
Aaron Strouse <aaronstrouse@gmail.com>
2017-04-26 23:10:22 -0400

    added lists to Jade templates

--- src/app.js
+++ src/app.js
@@ -3,29 +3,28 @@
 var express = require('express'),
 	  posts = require('./mock/posts.json');
 
+var postsLists = Object.keys(posts).map(function(value) {
+							         return posts[value]})
+
 var app = express();
-//middleware is the logic in the "middle" time between a request made by a client but before it arrives at a route.
-// Express static method.  One of few places we access the express module directly, instead of teh app variable.
-//Paths are relative to node process that is running the file. 
+
 app.use('/static', express.static(__dirname + '/public'))
 
 app.set('view engine', 'jade');
-app.set('views', __dirname + '/templates')
+app.set('views', __dirname + '/templates');
 
 app.get('/', function(req, res){
-	res.render('index')
+	res.render('index');
 });
 
 app.get('/blog/:title?', function(req, res){ 
 	var title = req.params.title;
 	if (title === undefined) {
 		res.status(503);
-		res.send("This page is under construction!");
+		res.render('blog', {posts: postsLists})
 	} else {
-		var post = posts[title] || {};//The || {} is common pattern in JS to avoid giving an undefined variable error.
-		res.render('post', { post: post });
-		//The second paramater above allows dynamic content to be added.
-		//The #{post.title/description} field in the post.jade file allows this. 
+		var post = posts[title] || {};
+		res.render('post', { post: post});
 	}
 });
 
